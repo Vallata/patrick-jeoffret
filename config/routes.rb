9b1e9cb4ddf826571148c5523c7b11ac967c3c85
Alizee Vallat <alizee.v@live.fr>
2023-07-28 12:00:33 +0200

    save

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get '/about', to: 'pages#about'
   get '/contact', to: 'pages#contact'
   get '/thanks', to: 'pages#thanks'
-  resources :items, only: %i[new create index edit update destroy]
+  resources :items, only: %i[new show create index edit update destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
